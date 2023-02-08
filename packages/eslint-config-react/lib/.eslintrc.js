ac242c456eb3c9b8a63e8aa80819bee3a38b15ec
Rachel Opperman <oppermanre17@gmail.com>
2023-02-08 14:23:43 -0500

    Adds React ESLint config package

---
+++ packages/eslint-config-react/lib/.eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    node: true,
+    es2021: true,
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'array-bracket-spacing': ['warn', 'never'],
+    'arrow-parens': [
+      'warn',
+      'as-needed',
+      {
+        requireForBlockBody: true,
+      },
+    ],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'eol-last': ['warn', 'always'],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'warn',
+    'object-curly-spacing': ['warn', 'always'],
+    quotes: [
+      'warn',
+      'single',
+      {
+        allowTemplateLiterals: true,
+      },
+    ],
+    semi: ['warn', 'never'],
+    'space-before-function-paren': [
+      'error',
+      {
+        named: 'never',
+        anonymous: 'always',
+        asyncArrow: 'always',
+      },
+    ],
+  },
+}
