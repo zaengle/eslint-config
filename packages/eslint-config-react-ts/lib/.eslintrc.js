f79f43ca12672073b2b1dfec5b0bb2c6597ddc3c
Jasmine Tracey <jasmine@zaengle.com>
2023-10-24 14:04:56 -0500

    added react-ts

---
+++ packages/eslint-config-react-ts/lib/.eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    node: true,
+    es2021: true,
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'array-bracket-spacing': ['warn', 'never'],
+    'arrow-parens': [
+      'warn',
+      'as-needed',
+      {
+        requireForBlockBody: true,
+      },
+    ],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'eol-last': ['warn', 'always'],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'warn',
+    'object-curly-spacing': ['warn', 'always'],
+    quotes: [
+      'warn',
+      'single',
+      {
+        allowTemplateLiterals: true,
+      },
+    ],
+    semi: ['warn', 'never'],
+    'space-before-function-paren': [
+      'error',
+      {
+        named: 'never',
+        anonymous: 'always',
+        asyncArrow: 'always',
+      },
+    ],
+  },
+}
