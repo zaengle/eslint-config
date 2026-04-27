a79f26d7ba73e02a2c14bbcdbb2e69c7a59166bb
Jenny Medeiros <jennymedeiros@Mac.medeiroshome>
2026-04-27 18:40:27 -0400

    #3: Update to ESLint 10 with flat config

--- packages/eslint-config-vue-ts/lib/index.js
+++ packages/eslint-config-vue-ts/lib/index.js
@@ -1,3 +1,70 @@
-const eslintrc = require('./.eslintrc.js')
+const js = require('@eslint/js')
+const globals = require('globals')
+const pluginVue = require('eslint-plugin-vue')
+const tsPlugin = require('@typescript-eslint/eslint-plugin')
+const tsParser = require('@typescript-eslint/parser')
 
-module.exports = eslintrc
+module.exports = [
+  js.configs.recommended,
+  ...tsPlugin.configs['flat/recommended'],
+  ...pluginVue.configs['flat/vue3-recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'array-bracket-spacing': ['warn', 'never'],
+      'arrow-parens': ['warn', 'as-needed', { requireForBlockBody: true }],
+      'comma-dangle': ['warn', 'always-multiline'],
+      indent: [
+        'error',
+        2,
+        {
+          SwitchCase: 1,
+        },
+      ],
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'object-curly-spacing': ['warn', 'always'],
+      quotes: [
+        'warn',
+        'single',
+        {
+          allowTemplateLiterals: true,
+        },
+      ],
+      semi: ['warn', 'never'],
+      'space-before-function-paren': [
+        'error',
+        {
+          named: 'never',
+          anonymous: 'always',
+          asyncArrow: 'always',
+        },
+      ],
+      'vue/component-api-style': ['error', ['script-setup', 'composition']],
+      'vue/no-v-text-v-html-on-component': 'off',
+      'vue/max-attributes-per-line': [
+        'warn',
+        {
+          singleline: 1,
+          multiline: 1,
+        },
+      ],
+    },
+  },
+]
--- packages/eslint-config-vue/lib/index.js
+++ packages/eslint-config-vue/lib/index.js
@@ -1,3 +1,34 @@
-const eslintrc = require('./.eslintrc.js')
+const js = require('@eslint/js')
+const globals = require('globals')
+const pluginVue = require('eslint-plugin-vue')
 
-module.exports = eslintrc
+module.exports = [
+  js.configs.recommended,
+  ...pluginVue.configs['flat/vue3-recommended'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'array-bracket-spacing': ['warn', 'never'],
+      'arrow-parens': ['warn', 'as-needed', { requireForBlockBody: true }],
+      'comma-dangle': ['warn', 'always-multiline'],
+      'eol-last': ['warn', 'always'],
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-unused-vars': 'warn',
+      'object-curly-spacing': ['warn', 'always'],
+      quotes: ['warn', 'single', { allowTemplateLiterals: true }],
+      semi: ['warn', 'never'],
+      'space-before-function-paren': ['error', { named: 'never', anonymous: 'always', asyncArrow: 'always' }],
+      'vue/max-attributes-per-line': ['warn', { singleline: 1, multiline: 1 }],
+    },
+  },
+]
