8764532316af15716df8e5b4fa05f63b16797e4e
Tom Davies <tom@tomdavies.net>
2026-05-05 17:49:30 +0100

    Add test suite for eslint-config-vue-ts
    Includes fix for flat/vue3-recommended -> flat/recommended (same as vue package).

---
+++ packages/eslint-config-vue-ts/test/fixtures/invalid/OptionsApi.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>options api</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return { x: 1 }
+  },
+})
+</script>
---
+++ packages/eslint-config-vue-ts/test/fixtures/invalid/explicit-any.ts
@@ -0,0 +1,3 @@
+export function bad(x: any): any {
+  return x
+}
